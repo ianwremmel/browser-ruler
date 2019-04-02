254ed6708ae8f308716c060d65034757e8cf0481
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2019-04-01 21:09:16 -0700

    feat: use shipit instead of sem rel plugins

---
+++ .releaserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  verifyConditions: ['condition-circle'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/exec', {prepareCmd: 'npm run build'}],
+    ['@semantic-release/exec', {publishCmd: 'shipit chrome dist'}],
+    ['@semantic-release/exec', {publishCmd: 'shipit firefox dist'}],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'ruler.xpi',
+          },
+          {
+            path: 'ruler.zip',
+          },
+        ],
+      },
+    ],
+  ],
+};
