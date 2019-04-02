1031c783838464525ee689177dca04ceb3ab6666
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2019-04-01 22:42:26 -0700

    fix(release): use web-ext directly instead of shipit for firefox

--- .releaserc.js
+++ .releaserc.js
@@ -8,7 +8,21 @@ module.exports = {
       {prepareCmd: 'VERSION=${nextRelease.version} npm run build'},
     ],
     ['@semantic-release/exec', {publishCmd: 'shipit chrome dist'}],
-    ['@semantic-release/exec', {publishCmd: 'shipit firefox dist'}],
+    [
+      '@semantic-release/exec',
+      {
+        publishCmd: [
+          'web-ext',
+          'sign',
+          '--source-dir',
+          'dist',
+          '--api-key',
+          process.env.WEXT_SHIPIT_FIREFOX_JWT_ISSUER,
+          '--api-secret',
+          process.env.WEXT_SHIPIT_FIREFOX_JWT_SECRET,
+        ].join(' '),
+      },
+    ],
     ['@semantic-release/github'],
   ],
 };
